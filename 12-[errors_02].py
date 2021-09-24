9e24cf55545fee393aafbad556da2ea32fbc8062
shavkat_amirov <84470034+shavkat1717@users.noreply.github.com>
2021-09-24 23:51:21 +0500

    Add files via upload

---
+++ 12-[errors_02].py
@@ -0,0 +1,9 @@
+yosh =int(input("Yoshingiz nechida?\t => "))
+
+if yosh<=4 or yosh>=60:
+    narh = 0
+elif yosh < 18:
+    narh = 10000
+else:
+    narh = 20000
+print(f"Chipta {narh} so'm")
---
+++ 12-[errors_03].py
@@ -0,0 +1,8 @@
+x = float(input("Birinchi sonni kiriting: \t => "))
+y = float(input("Ikkinchi sonni kiriting: \t => "))
+if x==y:
+    print(f"{x}={y}")
+elif x<y:
+    print(f"{x}<{y}")
+else:
+    print(f"{x}>{y}")
