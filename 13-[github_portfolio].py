56073063f2dde080112fd4e4875c81d7fa98090e
shavkat_amirov <84470034+shavkat1717@users.noreply.github.com>
2021-09-26 23:12:11 +0500

    the next answers
    update templates

---
+++ 13-[github_portfolio].py
@@ -0,0 +1,10 @@
+#GitHub.com sahifasidan ro'yxatdan o'ting
+print("1. www.github.com saytidan ro'yxatdan o'tdim: \n https://github.com/shavkat1717\n")
+#Kompyuteringizga GitHub Desktop dasturini o'rnating
+print("2. Kompyuterga GitHub Desktop dasturini o'rnatdim.\n")
+#GitHubda Python darslari uchun yangi repository yarating
+print("3. GitHubda Python darslari uchun yangi repository yaratdim: \nhttps://github.com/shavkat1717/python_answers\n")
+#Shu vaqtgacha yozgan kodlaringizni yangi repositoryga yuklang
+print("4. Shu vaqtgacha yozgan kodlarimni yangi repositoryga yukladim, Masalan:\n https://github.com/shavkat1717/python_answers/blob/main/01-%5Bkerakli_dasturlar%5D.py\n")
+#Sahifamizga qo'yib borilayotgan mashg'ulotlarning javoblarini quyidagi repositorydan o'zingizga Clone qilib oling:
+print("5. Mashg'ulotlarning javoblarini o'zimga Clone qilib oldim.\n")
