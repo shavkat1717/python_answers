5e049769cd48e6075cacc68f011f5de7ba4dec63
shavkat_amirov <84470034+shavkat1717@users.noreply.github.com>
2021-09-29 21:43:54 +0500

    Add files via upload

---
+++ 17-[while_02].py
@@ -0,0 +1,20 @@
+savol = "Yoshingizni kiriting: "
+
+while True:
+    qiymat = input(savol)
+    if qiymat == 'exit' or qiymat == 'quit':
+        break
+    yosh = int(qiymat)
+    
+    if yosh<7:
+        narh = 2000
+    elif 7<=yosh<18:
+        narh = 3000
+    elif 18<=yosh<65:
+        narh = 10000
+    else: narh = 0
+    
+    if narh==0:
+        print("Sizga chipta bepul")
+    else:
+        print(f"Chipta {narh} so'm")
